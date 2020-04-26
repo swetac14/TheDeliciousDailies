088b5a7638bb30890c1dc9d2608019e528bdc0d5
Sweta Chauhan <sweta.chauhan14@gmail.com>
2020-04-26 13:19:59 -0500

    Add new ingredient functionality implemented

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,15 +7,19 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredient: Ingredient[] = [
+  ingredients: Ingredient[] = [
     new Ingredient('Chickpeas', 5),
     new Ingredient('Potatoes', 10),
     new Ingredient('Mint leaves', 10)
-
   ];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onIngredientAdded(addingredient: Ingredient) {
+    this.ingredients.push(addingredient);
   }
 
 }
